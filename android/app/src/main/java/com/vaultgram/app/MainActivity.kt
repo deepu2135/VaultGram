9b08ebbade790938f60a1995dda57f5ef25ac15b
deepu2135 <deepu2135@users.noreply.github.com>
2026-08-01 01:47:32 +0530

    Add GitHub Actions CI/CD workflow and Android app module for release APK

---
+++ android/app/src/main/java/com/vaultgram/app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.vaultgram.app
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+            allowContentAccess = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://127.0.0.1:8000")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
